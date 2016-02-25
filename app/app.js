1f51b5d8e9307e1dd1fb0ff9c4b036e3089183e2
ElizabethKMathew <elizabeth@qburst.com>
2016-02-25 18:35:48 +0530

    modified userdisplay

--- app/app.js
+++ app/app.js
@@ -2,16 +2,16 @@ var app = angular.module('flickerApp', ['ngRoute']);
 app.config(function($routeProvider) 
             {
                 $routeProvider.when('/dashboard', {
-                    templateUrl: '../views/dashboard.html',
+                    templateUrl: 'views/dashboard.html',
                     controller: 'dashboardController'
                 }).when('/user',{
-                    templateUrl: '../views/users.html',
+                    templateUrl: 'views/users.html',
                     controller: 'userController'
                 }).when('/user/:uid',{
-                    templateUrl: '../views/user-details.html',
+                    templateUrl: 'views/user-details.html',
                     controller: 'userDetailsController'
                 }).when('/user/:uid/image/:id',{
-                    templateUrl: '../views/img-details.html',
+                    templateUrl: 'views/img-details.html',
                     controller: 'userDetailsController'
                 });
                 
